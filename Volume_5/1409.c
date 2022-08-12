70f57c6840fba9904228d075b390e566a9639d27
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2022-08-12 16:02:17 +0600

    Add files via upload

---
+++ Volume_5/1409.c
@@ -0,0 +1,15 @@
+// Two Gangsters
+
+#include <stdio.h>
+
+int main()
+{
+    int harry, larry;
+
+    scanf("%d %d", &harry, &larry);
+
+    printf("%d %d\n", 10 - harry, 10 - larry);
+    // maximum 10 cans can be on the log
+
+    return 0;
+}
