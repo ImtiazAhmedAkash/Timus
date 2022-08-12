557d471fb6276af23ce135ce2ae070696555f136
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2022-08-12 23:32:47 +0600

    Update 1293.c

--- Volume_3/1293.c
+++ Volume_3/1293.c
@@ -8,9 +8,9 @@ int main()
 
     scanf("%d %d %d", &number, &height, &width);
 
-    long int total = number * 2 * (height * width);
     // 2 is multiplied because we have to calculate
     // the dimension of both sides of the panel
+    long int total = number * 2 * (height * width);
 
     printf("%ld\n", total);
 
